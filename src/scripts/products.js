c9a50f23e3cc052220048d8e2d681bfdffbb7deb
Luiz Souza <henryque.souz4@gmail.com>
2020-12-02 18:40:32 -0400

    feat: add to cart finished

--- src/scripts/products.js
+++ src/scripts/products.js
@@ -1,5 +1,32 @@
 import Cookie from 'js-cookie'
 
-Cookie.set('name', 'luiz')
-const name = Cookie.get('name')
-console.log(name)
+const products = document.querySelectorAll('.js-product-card')
+
+function addToCart (productId) {
+  const cartQuantity = document.querySelector('.js-cart-quantity')
+
+  const productsCart = Cookie.get('products-cart') || []
+
+  const products = typeof productsCart === 'string'
+    ? JSON.parse(productsCart)
+    : productsCart
+
+  const isProductExists = products.includes(productId)
+
+  if (!isProductExists) {
+    products.push(productId)
+    Cookie.set('products-cart', JSON.stringify(products), { expires: 2 })
+    cartQuantity.innerHTML = Number(cartQuantity.innerText) + 1
+  }
+}
+
+function addEventInProducts (products) {
+  products.forEach(element => {
+    const addToCartButton = element.querySelector('.js-cart-button')
+    const productId = element.querySelector('.js-product-id').value
+
+    addToCartButton.addEventListener('click', () => addToCart(productId))
+  })
+}
+
+addEventInProducts(products)
